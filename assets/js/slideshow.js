ecaebb3e18a8324296e78a903c4529bef4eb0ce8
lilychuang78 <lilychuang78@gmail.com>
2020-11-26 17:06:45 +0000

    add slideshow.js

---
+++ assets/js/slideshow.js
@@ -0,0 +1,10 @@
+ /*credit: https://css-tricks.com/snippets/jquery/simple-auto-playing-slideshow/*/
+$(".img-case:gt(0)").hide();
+setInterval(function() {
+  $('.img-case:first')
+    .fadeOut(3000)
+    .next()
+    .fadeIn(3000)
+    .end()
+    .appendTo('#slideshow');
+}, 4000);
