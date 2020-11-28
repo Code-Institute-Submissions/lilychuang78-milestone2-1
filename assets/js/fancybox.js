21b393c23d4da54acd6fceecdafeac7ae94af604
lilychuang78 <lilychuang78@gmail.com>
2020-11-28 22:21:13 +0000

    use fantacybox api for cuisine photos instead

---
+++ assets/js/fancybox.js
@@ -0,0 +1,14 @@
+ /*credit: https://codepen.io/fancyapps/pen/vzoPMB?editors=1010*/
+
+$('[data-fancybox="images"]').fancybox({
+  buttons : [ 
+    'slideShow',
+    'share',
+    'zoom',
+    'fullScreen',
+    'close'
+  ],
+  thumbs : {
+    autoStart : true
+  }
+});
--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,4 +1,5 @@
  /*credit: https://css-tricks.com/snippets/jquery/simple-auto-playing-slideshow/*/
+ 
 $(".img-case:gt(0)").hide();
 setInterval(function() {
   $('.img-case:first')
