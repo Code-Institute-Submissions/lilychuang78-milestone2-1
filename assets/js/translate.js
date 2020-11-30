924efa450aaf6df7250911fb57c5fe68b3604c29
lilychuang78 <lilychuang78@gmail.com>
2020-11-30 16:56:45 +0000

    add google translation api to language section

---
+++ assets/js/translate.js
@@ -0,0 +1,12 @@
+//credit: inspired by https://www.w3schools.com/howto/howto_google_translate.asp
+let translate;
+function googleTranslateElementInit() {
+    translate =  new google.translate.TranslateElement({
+        pageLanguage: 'en',
+        includedLanguages: 'zh-TW,en',
+        autoDisplay:true
+    },
+         'google_translate_element'
+    );
+}
+
