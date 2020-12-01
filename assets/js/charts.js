187e3e14e9565280c45308a0dfd65336d8e2fc4c
lilychuang78 <lilychuang78@gmail.com>
2020-12-01 13:02:46 +0000

    add a chart in business section

--- assets/js/charts.js
+++ assets/js/charts.js
@@ -1,3 +1,4 @@
+//POPULATION SECTION chart1 
 //population data taken from https://www.ris.gov.tw/app/portal/346
 var ctx = document.getElementById("chart1").getContext('2d');
 var chart1 = new Chart(ctx, {
@@ -12,5 +13,65 @@ var chart1 = new Chart(ctx, {
             backgroundColor: '#FF7F50',
             borderWidth: 1
         }]},
-    options: { responsive: true, maintainAspectRatio: false}
+    options: {
+        responsive: true,
+        maintainAspectRatio: false, //the canvas is resized but becomes blurry
+        scales: {
+            yAxes: [{
+                ticks: {
+                    callback:function(value, index, values){
+                        return value/1e6+' million'
+                    }
+                }
+            }]
+        }
+    }
+});
+
+
+//BUSINESS SECTION chart2
+//survey data taken from https://www.internations.org/press/press-release/expat-insider-2019-survey-reveals-the-best-and-worst-destinations-to-live-and-work-in-2019-39881
+var data = {
+  labels: [
+    "Affordable healthcare",
+    "Quality of medical care",
+    "Personal safety",
+    "Easy to settle down",
+    "Friendly locals",
+    "Career prospects",
+    "Learning local languages"
+  ],
+  datasets: [
+    {
+      label: "Taiwan",
+      backgroundColor: "lightgreen",
+      borderColor: "green",
+      borderWidth: 1,
+      data: [89, 92, 96, 78, 88, 65, 52]
+    },
+    {
+      label: "Global",
+      backgroundColor: "pink",
+      borderColor: "red",
+      borderWidth: 1,
+      data: [55, 65, 81, 59, 68, 55, 45]
+
+    }
+  ]
+};
+
+var options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  title: {
+    display: true,
+    text: "2019 Expat Insider Survey "
+  }
+}
+
+var ctx = document.getElementById("chart2").getContext("2d");
+window.myBar = new Chart(ctx, {
+type: "bar",
+option: options,
+data: data,
 });
