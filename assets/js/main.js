c11412c3ac2276bad28f4abf7605a08ed6d98f21
lilychuang78 <lilychuang78@gmail.com>
2020-12-07 16:32:41 +0000

    remove jquery function of tab in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,18 +8,3 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-//tab hover effect:
-//credit: https://www.w3schools.com/howto/howto_js_hover_tabs.asp
-function openTab(evt, tabTitle) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(tabTitle).style.display = "block";
-  evt.currentTarget.className += " active";
-}
